bd4eb607d8510428d8ad802763c96c888b2126bc
Nozo <nozomi.pine@gmail.com>
2022-01-25 21:42:43 +0900

    Initialize with wio-examples

--- wio-examples/examples/6-7-i2c_read_device_id.rs
+++ wio-examples/examples/6-7-i2c_read_device_id.rs
@@ -1,25 +1,28 @@
+//! 6-7 加速度センサ/I2Cのサンプルコードです。
+//! I2CでLIS3DHからデバイスIDを取得します。
+//!
+//! ### 実行方法
+//! ```sh
+//! $ cargo hf2 --example 6-7-i2c_read_device_id
+//! ```
+
 #![no_std]
 #![no_main]
 
+use panic_halt as _;
 use wio_terminal as wio;
 
+use core::fmt::Write;
 use wio::entry;
 use wio::hal::clock::GenericClockController;
 use wio::hal::gpio::*;
 use wio::hal::sercom::*;
 use wio::pac::Peripherals;
 use wio::prelude::*;
-use core::panic::PanicInfo;
-use core::fmt::Write;
-
-static mut UART: Option<
-    UART2<Sercom2Pad1<Pb27<PfC>>, Sercom2Pad0<Pb26<PfC>>, (), ()>
-> = None;
 
 #[entry]
 fn main() -> ! {
     let mut peripherals = Peripherals::take().unwrap();
-
     let mut clocks = GenericClockController::with_external_32kosc(
         peripherals.GCLK,
         &mut peripherals.MCLK,
@@ -29,31 +32,18 @@ fn main() -> ! {
     );
 
     let mut sets = wio::Pins::new(peripherals.PORT).split();
-    let serial = sets.uart.init(
+    // UARTドライバオブジェクトを初期化する
+    let mut serial = sets.uart.init(
         &mut clocks,
         115200.hz(),
         peripherals.SERCOM2,
         &mut peripherals.MCLK,
-        &mut sets.port
+        &mut sets.port,
     );
 
-    unsafe {
-        UART = Some(serial);
-        writeln!(UART.as_mut().unwrap(), "hello world").unwrap();
-    }
+    // TODO: I2Cドライバオブジェクトを初期化する
 
-    // panic!
-    let none: Option<usize> = None;
-    none.unwrap();
-
-    loop {}
-}
-
-#[panic_handler]
-fn panic(info: &PanicInfo) -> ! {
-    unsafe {
-        writeln!(UART.as_mut().unwrap(), "panic: {}", info).ok();
-    }
+    // TODO: LIS3DHのデバイスIDを取得する
 
     loop {}
 }
