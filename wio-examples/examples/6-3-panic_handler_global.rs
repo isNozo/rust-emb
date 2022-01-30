c77430c4a7ba03962e6fcd34b43e4b9a275eecb1
Nozo <nozomi.pine@gmail.com>
2022-01-30 21:46:35 +0900

    Add echo and panic_handler

--- wio-examples/examples/6-3-panic_handler_global.rs
+++ wio-examples/examples/6-3-panic_handler_global.rs
@@ -45,12 +45,25 @@ fn main() -> ! {
         &mut sets.port,
     );
 
-    // TODO: グローバル変数に格納されているNoneをSomeで上書きする
+    // グローバル変数に格納されているNoneをSomeで上書きする
+    unsafe {
+        UART = Some(serial);
+        writeln!(UART.as_mut().unwrap(), "set serial to global UART.").unwrap();
+    }
 
-    // TODO: わざとNoneをunwrap()してパニックを発生させる
+    // わざとNoneをunwrap()してパニックを発生させる
+    let none: Option<usize> = None;
+    none.unwrap();
 
     loop {}
 }
 
-// TODO: パニックハンドラを実装する
+// パニックハンドラを実装する
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    unsafe {
+        writeln!(UART.as_mut().unwrap(), "panic: {}", info).ok();
+    }
+    loop {}
+}
 
